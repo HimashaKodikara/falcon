d46ae9427cbd5de33753ec45e0eda19745701362
19840769 <kodikarah.21@uom.lk>
2024-07-06 23:01:17 +0530

    chore: Update image gallery component and CSS styles

--- src/App.js
+++ src/App.js
@@ -2,19 +2,45 @@
 import React, { useState } from 'react';
 import './App.css';
 import Gallery from './Gallery';
-import F1 from './images/F1.jpg';
+import F1 from './images/EXMO/F1.jpg';
+import F2 from './images/EXMO/F2.jpg';
+import F3 from './images/EXMO/F3.jpg';
+import F4 from './images/EXMO/F4.jpg';
+import E1 from './images/EV/E1.jpg';
+import E2 from './images/EV/E2.jpg';
+import E3 from './images/EV/E3.jpg';
+import E4 from './images/EV/E4.jpg';
+import E5 from './images/EV/E5.jpg';
+import E6 from './images/EV/E6.jpg';
+import B1 from './images/Formula/B1.jpg';
+import B2 from './images/Formula/B1.jpg';
+import B3 from './images/Formula/B1.jpg';
+import B4 from './images/Formula/B1.jpg';
+
 
 const App = () => {
-  const categories = ['EXMO', 'Technology', 'Architecture', 'People', 'Animals'];
+  const categories = ['EXMO', 'EV Exhibition', 'Formula Bharat', 'People', 'Animals'];
 
   const images = [
-    { src: F1, description: 'A beautiful forest', category: 'Nature' },
-    { src: 'tech1.jpg', description: 'Modern technology', category: 'Technology' },
-    // Add more images for other categories
+    { src: F1, description: 'Falcon on road', category: 'EXMO' },
+    { src: F2, description: 'Conferance', category: 'EXMO' },
+    { src: F3, description: 'Conferance', category: 'EXMO' },
+    { src: F4, description: 'Conferance', category: 'EXMO' },
+    { src: E1, description: 'Modern technology', category: 'EV Exhibition' },
+    { src: E2, description: 'Modern technology', category: 'EV Exhibition' },
+    { src: E3, description: 'Modern technology', category: 'EV Exhibition' },
+    { src: E4, description: 'Modern technology', category: 'EV Exhibition' },
+    { src: E5, description: 'Modern technology', category: 'EV Exhibition' },
+    { src: E6, description: 'Modern technology', category: 'EV Exhibition' },
+    { src: B1, description: 'Modern technology', category: 'Formula Bharat' }, 
+     { src: E6, description: 'Modern technology', category: 'Formula Bharat' },
+     { src: E6, description: 'Modern technology', category: 'Formula Bharat' },
+     { src: E6, description: 'Modern technology', category: 'Formula Bharat' },
+     { src: E6, description: 'Modern technology', category: 'Formula Bharat' },
   ];
 
   return (
-    <div className="App w-8">
+    <div className="App ">
       <Gallery categories={categories} images={images}  />
     </div>
   );
--- src/Modal.js
+++ src/Modal.js
@@ -4,10 +4,18 @@ import React from 'react';
 const Modal = ({ image, onClose }) => {
   return (
     <div className="modal fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="modal-content bg-white p-4 rounded-lg shadow-lg relative">
-        <button className="absolute top-2 right-2 bg-red-500 text-black p-2 px-6 font-bold rounded-3xl" onClick={onClose}>X</button>
+      <div className="modal-content bg-white rounded-lg shadow-lg relative">
+        <button
+          className="absolute top-2 right-2 bg-red-500 rounded-md p-2 text-white hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+          onClick={onClose}
+        >
+          <span className="sr-only">Close menu</span>
+          <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
         <img src={image.src} alt={image.description} className="mx-auto max-h-full max-w-3xl rounded-lg" />
-
+        <p className="mt-4 text-center">{image.description}</p>
       </div>
     </div>
   );
